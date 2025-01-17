8773c23d1d142a969334353cf46793cb0532f482
xiangyang <15138778469@163.com>
2025-01-17 17:07:15 +0800

    a

--- src/main/java/com/xiangyang/rocketmq/consumer/RocketMQConsumerContainer.java
+++ src/main/java/com/xiangyang/rocketmq/consumer/RocketMQConsumerContainer.java
@@ -180,7 +180,9 @@ public class RocketMQConsumerContainer implements BeanFactoryAware, ApplicationC
         Properties props = new Properties();
         props.setProperty(PropertyKeyConst.AccessKey, properties.getAccessKey());
         props.setProperty(PropertyKeyConst.SecretKey, properties.getSecretKey());
-        props.setProperty(PropertyKeyConst.INSTANCE_ID, properties.getInstanceId());
+        if(properties.getInstanceId() !=null){
+            props.setProperty(PropertyKeyConst.INSTANCE_ID, properties.getInstanceId());
+        }
         props.setProperty(PropertyKeyConst.GROUP_ID, annotation.consumerGroup());
         props.setProperty(PropertyKeyConst.NAMESRV_ADDR,properties.getNameServerAddr());
 
