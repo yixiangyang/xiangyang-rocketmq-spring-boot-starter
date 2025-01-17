d857f0b65edc69237821e3b691f531029734b4be
xiangyang <15138778469@163.com>
2025-01-17 16:11:08 +0800

    aa

--- src/main/java/com/xiangyang/rocketmq/config/RocketMQAutoConfiguration.java
+++ src/main/java/com/xiangyang/rocketmq/config/RocketMQAutoConfiguration.java
@@ -2,23 +2,32 @@ package com.xiangyang.rocketmq.config;
 
 import com.xiangyang.rocketmq.consumer.RocketMQConsumerContainer;
 import com.xiangyang.rocketmq.producer.RocketMQProducerTemplate;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 
 
-@Configuration
+@AutoConfiguration
 @EnableConfigurationProperties(RocketMQProperties.class)
-@ConditionalOnProperty(prefix = "rocketmq", value = "enabled", havingValue = "true")
+@ConditionalOnProperty(prefix = "rocketmq", value = "enabled", havingValue = "true", matchIfMissing = true)
+@Order(Ordered.LOWEST_PRECEDENCE - 100)
+@ComponentScan(basePackages = "com.xiangyang.rocketmq")
 public class RocketMQAutoConfiguration {
 
     @Bean
+    @ConditionalOnMissingBean
     public RocketMQProducerTemplate rocketMQProducerTemplate(RocketMQProperties properties) {
         return new RocketMQProducerTemplate(properties);
     }
 
     @Bean
+    @ConditionalOnMissingBean
     public RocketMQConsumerContainer rocketMQConsumerContainer(RocketMQProperties properties) {
         return new RocketMQConsumerContainer(properties);
     }
